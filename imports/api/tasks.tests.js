56bcbd6ca919567742e46b106b10b968f6339195
dannyvassallo <danielvassallo87@gmail.com>
2016-10-31 11:00:55 -0400

    using expect in tests

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -1,7 +1,7 @@
 /* eslint-env mocha */
 import { Meteor } from 'meteor/meteor';
 import { Random } from 'meteor/random';
-import { assert } from 'meteor/practicalmeteor:chai';
+import { assert, expect } from 'meteor/practicalmeteor:chai';
 import { resetDatabase } from 'meteor/xolvio:cleaner';
 
 import { Tasks } from './tasks.js';
@@ -20,6 +20,7 @@ if (Meteor.isServer) {
           createdAt: new Date(),
           owner: userId,
           username: 'tmeasday',
+          private: true
         });
       });
 
@@ -35,7 +36,29 @@ if (Meteor.isServer) {
         deleteTask.apply(invocation, [taskId]);
 
         // Verify that the method does what we expected
-        var assertion = assert.equal(Tasks.find().count(), 0);
+        var count = Tasks.find().count();
+        console.log("Tasks count is " + count + " should be 0");
+        var assertion = assert.equal(count, 0);
+
+        return assertion;
+
+      });
+
+      it('cannot delete unowned task', function() {
+        // Find the internal implementation of the task method so we can
+        // test it in isolation
+        const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+        // //
+        // // const invocation =  Random.id();
+        // // Run the method with `this` set to the fake invocation
+        function attemptDeletion(){
+          deleteTask.apply(Random.id(), [taskId]);
+        }
+
+        // Verify that the method does what we expected
+        var count = Tasks.find().count();
+        console.log("Tasks count is " + count + " should be 1");
+        var assertion = expect(attemptDeletion).to.throw(Error);
 
         return assertion;
 
