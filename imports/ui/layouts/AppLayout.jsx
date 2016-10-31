e44ee4a02ccc15ba4016fae2a20502810edfc3d5
dannyvassallo <danielvassallo87@gmail.com>
2016-10-31 16:24:13 -0400

    removed old accounts ui info

--- imports/ui/layouts/AppLayout.jsx
+++ imports/ui/layouts/AppLayout.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import NavBar from '../components/NavBar.jsx';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AccountsUI from '../components/AccountsUIWrapper.jsx';
 import Store from '../../reducers/index.js';
 
 class AppLayout extends React.Component {
@@ -33,14 +32,6 @@ class AppLayout extends React.Component {
       <MuiThemeProvider>
         <div>
           <NavBar {...this.state}/>
-          <div className="container">
-            <div className="row-fluid">
-              <div className="col-xs-12">
-                <p>Test</p>
-                <AccountsUI />
-              </div>
-            </div>
-          </div>
           {this.props.children && React.cloneElement(this.props.children, this.state)}
         </div>
       </MuiThemeProvider>
