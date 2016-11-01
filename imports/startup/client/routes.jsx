6e5f06339726a6c46433b4d453d02a228f01af2c
dannyvassallo <danielvassallo87@gmail.com>
2016-11-01 12:30:22 -0400

    refactored 404

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -7,7 +7,7 @@ import AppLayout from '../../ui/layouts/AppLayout.jsx';
 import { Index } from '../../ui/pages/Index.jsx';
 // When you export default you DON'T need {} otherwise, you will.
 import TasksPageContainer from '../../ui/containers/TasksPageContainer.jsx';
-import { NotFound } from '../../ui/pages/NotFound.jsx';
+import NotFound from '../../ui/pages/NotFound.jsx';
 import SignUpPage from '../../ui/pages/SignUpPage.jsx';
 import LoginPage from '../../ui/pages/LoginPage.jsx';
 
