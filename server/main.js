34c83bcc924fa0b7c9603685f8657a191eb7f40f
dannyvassallo <danielvassallo87@gmail.com>
2016-11-07 13:53:23 -0500

    added example settings.json file

--- server/main.js
+++ server/main.js
@@ -1,1 +1,8 @@
 import '../imports/api/tasks.js';
+
+Meteor.startup(() => {
+  // create admin from settings
+  if (Meteor.users.findOne(Meteor.settings.adminId)){
+    Roles.addUsersToRoles(Meteor.settings.adminId, ['admin']);
+  }
+});
